35279ca9ee7d42c2965203f900ff86f21d185ef4
Dominic Serrano <dmsrojo@gmail.com>
2017-03-29 14:20:47 -0500

    Views are now extending layout.pug.

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-// Middleswares
+// Express config
 app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'pug');
+// Middleswares
+app.use(express.static(__dirname + 'public'));
 
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.use((req, res, next) => {
+  res.render('404');
+});
 
 app.listen(app.get('port'), () => {
   console.log(`listening on port ${app.get('port')}`);
