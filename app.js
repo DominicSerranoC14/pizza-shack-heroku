59bc151e79c893de8c5484981795650dcd3ac0e7
Dominic Serrano <dmsrojo@gmail.com>
2017-03-30 12:58:35 -0500

    Cleaned up app.js

--- app.js
+++ app.js
@@ -12,14 +12,18 @@ app.set('view engine', 'pug');
 // Middleswares
 app.use(express.static('public'));
 
+
 // Routes
 app.use(require('./routes/'));
+
 
 // 404 Middleware
 app.use((req, res, next) => {
   res.render('404');
 });
 
+
+// Listen on the set port
 app.listen(app.get('port'), () => {
   console.log(`listening on port ${app.get('port')}`);
 });
